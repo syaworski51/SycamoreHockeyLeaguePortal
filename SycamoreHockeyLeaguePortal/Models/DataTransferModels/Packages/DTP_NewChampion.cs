d869ac86ed99e394efc8294dc6f32911c33430a3
syaworski51 <88213942+syaworski51@users.noreply.github.com>
2025-07-07 15:24:11 -0400

    Reorganize folder for Data Transfer Objects

---
+++ SycamoreHockeyLeaguePortal/Models/DataTransferModels/Packages/DTP_NewChampion.cs
@@ -0,0 +1,8 @@
+﻿namespace SycamoreHockeyLeaguePortal.Models.DataTransferModels.Packages
+{
+    public class DTP_NewChampion
+    {
+        public Champion Champion { get; set; }
+        public List<ChampionsRound> Rounds { get; set; }
+    }
+}
